d445dbb3467b7c30eb690da7e524511a2045040c
Vidlec <vidlec@gmail.com>
2017-05-16 13:04:02 +0200

    commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var request = require("request");
+function client(url, user, password) {
+    this.url = url;
+    this.user = user;
+    this.password = password;
+    this.auth = null;
+    this.method = "POST";
+    this.id = 1;
+    this.call = (method, params) => {
+        return new Promise((resolve, reject) => {
+            var options = {
+                uri: this.url,
+                method: this.method,
+                json: {
+                    auth: this.auth,
+                    id: this.id,
+                    jsonrpc: "2.0",
+                    method: method,
+                    params: params
+                }
+            };
+            request(options, (error, response, body) => {
+                (!error && response.statusCode == 200) ? resolve(body): reject(error);
+            });
+        });
+    }
+    this.login = () => {
+        return new Promise((resolve, reject) => {
+            this.call("user.login", {
+                user: this.user,
+                password: this.password
+            }).then((body) => {
+                this.auth = body.result;
+                resolve("Logged in");
+            }).catch((error) => console.log(error));
+        })
+    }
+    this.logout = () => {
+        return new Promise((resolve, reject) => {
+            this.call("user.logout", []).then((body) => {
+                this.auth = body.result;
+                resolve("Logged out");
+            }).catch((error) => console.log(error));
+        })
+    }
+}
+module.exports = client;
